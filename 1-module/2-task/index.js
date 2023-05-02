a34a149333e4273a7082f2770ddfe8b7c180f217
Anton Cherkashchenko <cherkashchenko31@gmail.com>
2023-05-02 12:52:07 +0700

    First Commit

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -11,7 +11,7 @@ function print(text) {
  */
 function isValid(name) {
   // ваш код...
-  if (name && name.trim().length >= 4 && !name.includes(" "))
+  if (name && name.length >= 4 && !name.includes(" "))
   {
 return true;
   }
