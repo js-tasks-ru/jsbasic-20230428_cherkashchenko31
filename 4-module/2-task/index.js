31d88f49aab0301a9e6043e5999669c30238656d
Anton Cherkashchenko <cherkashchenko31@yandex.ru>
2023-05-25 18:28:19 +0700

    5 module resolved

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,14 @@
 function makeDiagonalRed(table) {
   // ваш код...
-}
+    let rows = table.rows;
+    let rowCount = rows.length;
+    let colCount = rows[0].cells.length;
+    let diagonalCount = Math.min(rowCount, colCount);
+    for (let i = 0; i < diagonalCount; i++) {
+      rows[i].cells[i].style.backgroundColor = 'red';
+      rows[i].cells[i].style.textDecoration = 'line-through';
+
+    }
+  }
+
+
